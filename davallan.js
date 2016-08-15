14fe07cc6e67a0e332a06c0aa1ffa67a4443fc10
David Laptop <d.shafer@bargreen.com>
2016-08-14 17:42:24 -0700

    set time js

--- davallan.js
+++ davallan.js
@@ -3,12 +3,14 @@
 $(document).ready(function(){
    
     var welcome = ['H','e','l','l','o',' ','T','h','e','r','e','!'];
+    var time = 500;
     console.log(welcome[1]);
     
     function writeWelcome(){
         $.each(welcome, function(i){
             function writeLetter(){$('.main-pane').append(welcome[i])};
-            setTimeout(writeLetter,500);
+            setTimeout(writeLetter, time);
+            time += 500;
         });
     }
     
