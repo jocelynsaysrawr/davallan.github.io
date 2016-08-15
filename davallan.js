684fec8b50859aba8e4207dc7890eba3588ad71d
David Laptop <d.shafer@bargreen.com>
2016-08-14 17:23:06 -0700

    jquery

--- davallan.js
+++ davallan.js
@@ -1,1 +1,11 @@
 // this is davallan.js
+
+$(document).load(function(){
+   
+    var welcome = ['H','e','l','l','o',' ','T','h','e','r','e','!'];
+    
+    $.each(welcome, function(i){
+        $('.main-pane').append(welcome[i]);
+    });
+    
+});
