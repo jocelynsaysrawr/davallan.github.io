4b01c2462b848100335a59933fa31bd5934b967b
David Laptop <d.shafer@bargreen.com>
2016-08-14 20:30:22 -0700

    scroll js

--- davallan.js
+++ davallan.js
@@ -1,6 +1,8 @@
 // this is davallan.js
 
 $(document).ready(function(){
+    
+    window.scrollTo(0,1);
    
     var welcome = ['H','e','l','l','o',' ','T','h','e','r','e','!'];
     var time = 100;
