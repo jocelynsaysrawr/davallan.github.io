ae1b54919470a1e6605c53642a0295ed887e9ea2
David Laptop <d.shafer@bargreen.com>
2016-08-14 17:30:46 -0700

    js update

--- davallan.js
+++ davallan.js
@@ -5,8 +5,12 @@ $(document).ready(function(){
     var welcome = ['H','e','l','l','o',' ','T','h','e','r','e','!'];
     console.log(welcome[1]);
     
-    $.each(welcome, function(i){
-        $('.main-pane').append(welcome[i]).delay(5000);
-    });
+    function writeWelcome(){
+        $.each(welcome, function(i){
+            setTimeout({$('.main-pane').append(welcome[i])},5000);
+        });
+    }
+    
+    setTimeout(writeWelcome, 5000);
     
 });
