49c9ffac4ac320725c09011961f593a1faa3eab3
David Laptop <d.shafer@bargreen.com>
2016-08-16 16:26:52 -0700

    request full screen

--- davallan.js
+++ davallan.js
@@ -2,6 +2,15 @@
 
 $(document).ready(function(){
     
-   
+   addEventListener("click", function() {
+    var
+      el = document.documentElement
+    , rfs =
+           el.requestFullScreen
+        || el.webkitRequestFullScreen
+        || el.mozRequestFullScreen
+    ;
+    rfs.call(el);
+});
     
 });
