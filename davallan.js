95a1499ccde6e0688fc9b86547ba5dd1fb4483f1
David Laptop <d.shafer@bargreen.com>
2016-08-16 18:33:36 -0700

    js changes

--- davallan.js
+++ davallan.js
@@ -5,13 +5,15 @@ $(document).ready(function(){
     $('#bg-overlay').height($(window).height());
     
     $(document).on('click', '.enter-button', function(){
-        $(this).fadeOut('slow');
-       $('#bg-overlay').fullScreen(); 
+        $(this).fadeOut('fast');
+        $('body').fadeOut('fast');
+        $('#bg-overlay').fullScreen(); 
         
         setTimeout(
         function(){
             $('#bg-overlay').height($(window).height());
             $('.bottom-nav').css('bottom', '0').fadeIn();
+            $('body').fadeIn('slow');
         },1500);
         
     });
