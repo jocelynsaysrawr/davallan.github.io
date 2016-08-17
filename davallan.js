c68e3e4255ba71bd860a4b23f0220714862841f5
David Laptop <d.shafer@bargreen.com>
2016-08-16 17:25:13 -0700

    click title

--- davallan.js
+++ davallan.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     
     $('body, #bg-overlay').height($(window).height());
     
-    $(document).on('click', function(){
+    $(document).on('click', '.title', function(){
        $('#bg-overlay').fullScreen(); 
         $('body, #bg-overlay').height($(window).height());
     });
