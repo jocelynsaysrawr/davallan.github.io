86cdb0316c57d232912309c665b1f8a080bd3401
David Laptop <d.shafer@bargreen.com>
2016-08-23 21:02:07 -0700

    hmm

--- davallan.js
+++ davallan.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
     $.get('https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=2f9557344467c41a762ab15727fd46a1&photo_id=28165318402&format=rest&auth_token=72157672883510785-110320266b822668&api_sig=2d01f3e6f6ec0b8943dcf271431c8d09',
         function(e){
             console.log(e);
-            var source = e.find('size').first().attr('label');
+            var source = $('#document').find('size').first().attr('source');
             console.log(source);
         });
            
