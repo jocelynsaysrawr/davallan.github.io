3ddcfa7b7d1b4b447ebf1eb1a888e1d30328dd61
David Laptop <d.shafer@bargreen.com>
2016-08-23 21:30:12 -0700

    another

--- davallan.js
+++ davallan.js
@@ -22,8 +22,7 @@ $(document).ready(function(){
     
     $.get('https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=2f9557344467c41a762ab15727fd46a1&photo_id=28165318402&format=json',
         function(e){
-            var json = $.parseJSON(e);
-            console.log($.type(json));
+            console.log($.type($.parseJSON(e)));
             var sizes = json.sizes;
             sizes.each(function(i){
                 if (i.label == 'Original'){
