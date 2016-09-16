e860798d844b1729d5664d3232922dd2d29ad125
David Laptop <d.shafer@bargreen.com>
2016-09-15 21:55:15 -0700

    major posh

--- davallan.js
+++ davallan.js
@@ -2,10 +2,17 @@
 
 $(document).ready(function(){
     
+    $('.bottom-nav, .nav-back, .nav-forward').hide();
+    var step = 0;
     var textRelay = [
-        "Hello. I'm glad you are here! We've got a lot of catching up to do.",
-        "My name is David Shafer, and I am a front end web developer. I love what I do!"
+        "Hi. I'm David. I am a web developer specializing in visual experiences.",
+        "In a world where our senses are constantly bombarded, sometimes less is more...",
+        "I believe that information is easier to consume in bite-sized chunks...",
+        "You know... Like you are doing right now."
     ];
+    
+    var bgColorLoop = ['orangered','white','orange','white','royalblue','white','royalblue'];
+    var bgImageLoop = ['images/design-desk.jpg','images/design-desk.jpg','images/design-desk.jpg','images/design-desk.jpg'];
     
     $('#bg-overlay').height($(window).height());
     $('.text-box').hide();
@@ -18,23 +25,6 @@ $(document).ready(function(){
         $('#bg-overlay').fullScreen(); 
         $('.greeting').hide();
         
-        function writeText(e){
-            $('.text-box').html('');
-            $('.bottom-nav').hide();
-            var splitText = textRelay[e].split('');
-            console.log('split text');
-            console.log(splitText);
-            var splitInt = 0;
-            if (splitInt <= splitText.length-1){
-                setInterval(function(){
-                    $('.text-box').append(splitText[splitInt]);
-                    splitInt++;
-                }, 40);
-            } else {
-                $('.bottom-nav').fadeIn();
-            }
-        }
-        
         setTimeout(
         function(){
             $('#bg-overlay').height($(window).height());
@@ -43,18 +33,54 @@ $(document).ready(function(){
             $('.back-color').css('background-color','orangered');
         }, 1500);
     
-        
         setTimeout(function(){
             $('.text-box').fadeIn(); 
             writeText(0);
         }, 2500);
         
-        setTimeout(function(){
-             $('.back-color').css('background-color', 'royalblue');
-            $('.text-box').fadeIn(); 
-            writeText(1);
-        }, 15000);
-        
+    }); // End of Enter Click Function
+    
+    $('.nav-forward').click(function(){
+        step++;
+        $('.text-box').fadeIn(); 
+        writeText(step);
     });
+    
+    $('.nav-back').click(function(){
+        step--;
+        $('.text-box').fadeIn(); 
+        writeText(step);
+    });
+    
+    function writeText(e){
+        $('.back-color').css('background-color', bgColorLoop[e]);
+        
+        if(bgColorLoop[e] == 'white'){
+            var newE = e+1;
+            $('.text-box, .navcon').css('color', bgColorLoop[newE]);
+        } else {
+           $('.text-box, .navcon').css('color', 'white'); 
+        }
+//        if(bgImageLoop[e]) {
+//            $('.back-color').css('background-image', 'url(' + bgImageLoop[e] + ')');
+//        }
+        $('.text-box').html('');
+        $('.bottom-nav, .nav-forward, .nav-back').hide();
+        var splitText = textRelay[e].split('');
+        var splitInt = 0;
+        var id = setInterval(write, 40);
+
+        function write() {
+            if (splitInt == splitText.length){
+                clearInterval(id);
+                if(step >= 1){ $('.nav-back').show();}
+                if(step < textRelay.length-1) { $('.nav-forward').show();}
+                $('.bottom-nav').fadeIn();
+            } else {
+                $('.text-box').append(splitText[splitInt]);
+                splitInt++;
+            }
+        }
+    }
            
 });
