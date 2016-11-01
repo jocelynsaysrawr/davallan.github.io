7f154e1ab8dd3eb7a6f999b19f7b2e8f291ed006
David Laptop <d.shafer@bargreen.com>
2016-10-31 21:00:14 -0700

    js update scroll

--- davallan.js
+++ davallan.js
@@ -16,21 +16,37 @@ $(document).ready(function(){
                 scrollTop: target.offset().top
             }, 800);
             
-            $('.nav').each(function(i){
-                var order = $(this).data('order');
-                $(this).data('order', order+=1);
-            });
+//            $('.nav').each(function(i){
+//                var order = $(this).data('order');
+//                $(this).data('order', order+1);
+//            });
         } else if ($(this).hasClass('nav-up') && thisOrder >= minOrder) {
             $('html, body').animate({
                 scrollTop: target.offset().top
             }, 800);
-            $('.nav').each(function(i){
-                var order = $(this).data('order');
-                $(this).data('order', order-=1);
-            });
+//            $('.nav').each(function(i){
+//                var order = $(this).data('order');
+//                $(this).data('order', order-1);
+//            });
         } else {
             // do nothing
         }
     
     });
+    
+    $(document).on('scroll',function(){
+        $('.window-section').each(function(i){
+            var topDist = $(this).offset().top - $(window).scrollTop();
+            if (topDist >= -25 && topDist <= 100){
+                var thisVal = $(this).data('order');
+                if (thisVal+1 <= maxOrder){
+                    $('.nav-down').data('order', thisVal+1);
+                }
+                if (thisVal-1 >= minOrder){
+                    $('.nav-up').data('order', thisVal-1);
+                }
+            }
+        });
+    });
+    
 });
