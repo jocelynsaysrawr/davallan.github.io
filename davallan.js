064bd64c890b3039d81fec2297feb8d26ff1f324
David Laptop <d.shafer@bargreen.com>
2016-10-31 20:08:22 -0700

    changes

--- davallan.js
+++ davallan.js
@@ -1,5 +1,36 @@
 // this is davallan.js
 
 $(document).ready(function(){
-           
+    
+    var minOrder = $('.window-section').first().data('order');
+    var maxOrder = $('.window-section').last().data('order');
+    
+    console.log(minOrder + ' ' + maxOrder);
+    
+    $('.nav').click(function(event){
+        var eventTarget = $(event.target);
+        var thisOrder = $(this).data('order');
+        var target = $('.section'+thisOrder);
+        if ($(this).hasClass('nav-down') && thisOrder <= maxOrder){
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 800);
+            
+            $('.nav').each(function(i){
+                var order = $(this).data('order');
+                $(this).data('order', order+=1);
+            });
+        } else if ($(this).hasClass('nav-up') && thisOrder >= minOrder) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 800);
+            $('.nav').each(function(i){
+                var order = $(this).data('order');
+                $(this).data('order', order-=1);
+            });
+        } else {
+            // do nothing
+        }
+    
+    });
 });
