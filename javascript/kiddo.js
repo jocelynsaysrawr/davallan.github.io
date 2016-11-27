4a878ef5983e8530fcfaead38d0272b87d613fa6
David Laptop <d.shafer@bargreen.com>
2016-11-27 15:58:33 -0800

    changes

--- javascript/kiddo.js
+++ javascript/kiddo.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
 
-    $('.button').click(function(){
+    $('.audio-button').click(function(){
+
+        $('.audio').each(function(i){
+            this.pause();
+        });
+
         var thisID = $(this).attr('id');
         var audioID = thisID + 'audio';
         var audio = document.getElementById(audioID);
         console.log('play' + thisID);
+        audio.currentTime = 0;
         audio.play();
         return false;
     });
