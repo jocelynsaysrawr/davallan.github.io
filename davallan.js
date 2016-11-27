b67e29a27a6b86edc1dc570f43f41a65eaf11f20
David Laptop <d.shafer@bargreen.com>
2016-11-27 13:29:46 -0800

    anther change

--- davallan.js
+++ davallan.js
@@ -73,6 +73,7 @@ $(document).ready(function(){
         });
 
         $('#horn').click(function(){
+            console.log('play horn');
             $('#hornaudio').play();
             return false;
         });
