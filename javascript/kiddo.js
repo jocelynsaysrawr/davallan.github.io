e1382317e40ef66f0203cf7b2c0f8785204afb3b
David Laptop <d.shafer@bargreen.com>
2016-11-27 18:36:14 -0800

    fadeout button

--- javascript/kiddo.js
+++ javascript/kiddo.js
@@ -17,6 +17,7 @@ $(document).ready(function(){
 
     $('.full-it').click(function(){
         launchIntoFullscreen(document.documentElement);
+        $(this).fadeOut();
     });
 
     function launchIntoFullscreen(element) {
