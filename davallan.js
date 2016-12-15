8f888ee60709df043b50122c7869051a1fc96323
David Laptop <d.shafer@bargreen.com>
2016-12-14 22:26:27 -0800

    major changes.

--- davallan.js
+++ davallan.js
@@ -2,94 +2,22 @@
 
 $(document).ready(function(){
 
-    var minOrder = $('.section').first().data('order');
-    var maxOrder = $('.section').last().data('order');
 
-    function navHideShow() {
-        if($('.nav-up').data('order') == '0'){
-            $('.nav-up').hide();
-        } else {
-            $('.nav-up').show();
+    $.ajax({
+        url: "https://cdn.contentful.com/spaces/5n3n03bm8c2b/entries?access_token=47c9c8670688438ae1bec2f39c892d52fc1f045599e4a090ae983ff5c01df37f",
+        success: function(e){
+            var posts = e.items;
+            console.log(posts[0].fields.title);
+            $.each(posts, function(i){
+                var imgUrl = '';
+                $('body').append(
+                    '<div class="post-wrapper">' +
+                        '<span class="title">' + posts[i].fields.title + '</span>' +
+                        '<span class="body">' + posts[i].fields.body.replace(/\n/g, '<br/>') + '</span>' +
+                    '</div>'
+                );
+            });
         }
-
-        if($('.nav-down').data('order') == maxOrder){
-            $('.nav-down').hide();
-        } else {
-            $('.nav-down').show();
-        }
-    }
-
-    navHideShow();
-
-    console.log(minOrder + ' ' + maxOrder);
-
-    $('.nav').click(function(event){
-        var eventTarget = $(event.target);
-        var thisOrder = $(this).data('order');
-        var target = $('.section'+thisOrder);
-        if ($(this).hasClass('nav-down') && thisOrder <= maxOrder){
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 800);
-
-//            $('.nav').each(function(i){
-//                var order = $(this).data('order');
-//                $(this).data('order', order+1);
-//            });
-        } else if ($(this).hasClass('nav-up') && thisOrder >= minOrder) {
-            $('body, html').animate({
-                scrollTop: target.offset().top
-            }, 800);
-//            $('.nav').each(function(i){
-//                var order = $(this).data('order');
-//                $(this).data('order', order-1);
-//            });
-        } else {
-            // do nothing
-        }
-
     });
-
-    $(document).on('scroll',function(){
-        var topDoc = $('body,html').offset().top - $(window).scrollTop();
-        $('.section').each(function(i){
-            var topDist = $(this).offset().top - $(window).scrollTop();
-            if (topDist >= -25 && topDist <= 100){
-                var thisVal = $(this).data('order');
-                if (thisVal+1 <= maxOrder){
-                    $('.nav-down').data('order', thisVal+1);
-                }
-                if (thisVal-1 >= minOrder){
-                    $('.nav-up').data('order', thisVal-1);
-                }
-                if (thisVal == minOrder){
-                    $('.nav-up').fadeOut();
-                } else if (thisVal == maxOrder){
-                     $('.nav-down').fadeOut();
-                } else {
-                    $('.nav-down, .nav-up').fadeIn();
-                }
-            }
-        });
-
-    });
-
-
-
-
-//    function launchFullScreen(element) {
-//      if(element.requestFullScreen) {
-//        element.requestFullScreen();
-//      } else if(element.mozRequestFullScreen) {
-//        element.mozRequestFullScreen();
-//      } else if(element.webkitRequestFullScreen) {
-//        element.webkitRequestFullScreen();
-//      }
-//    }
-//
-//    $('.icofont').click(function(){
-//        console.log('full');
-//       launchFullScreen(document.body);
-//    });
 
 });
