2c7b25b17c0fde6b6fb23ace9707559e1cfc6d23
David Laptop <d.shafer@bargreen.com>
2017-01-10 22:13:35 -0800

    barcode scanner added

---
+++ barcode.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+
+    Quagga.init({
+        inputStream : {
+          name : "Live",
+          type : "LiveStream",
+          target: document.querySelector('#ls')    // Or '#yourElement' (optional)
+        },
+        decoder : {
+          readers : ["code_128_reader"]
+        }
+        }, function(err) {
+          if (err) {
+              console.log(err);
+              return
+          }
+          console.log("Initialization finished. Ready to start");
+          Quagga.start();
+    });
+
+});
