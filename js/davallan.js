cb0f898e4db9d3a99a5fccf788f58b4da83499f4
David Laptop <d.shafer@bargreen.com>
2017-08-04 21:34:13 -0700

    more changes

--- js/davallan.js
+++ js/davallan.js
@@ -1,5 +1,8 @@
 // this is davallan.js
 
 $(document).ready(function(){
-
+    $('.enter-button').click(function(){
+        var hft = $('.section.work').position().top;
+        $("html, body").animate({ scrollTop: hft +"px" });
+    });
 });
